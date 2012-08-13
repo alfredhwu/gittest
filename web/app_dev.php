3478198e122f935e93a1a56cfd6454db34ebe8e8
alfredhwu-ftfs <fujitsukefu@gmail.com>
2012-08-13 11:47:34 +0200

    ready to deploy

--- web/app_dev.php
+++ web/app_dev.php
@@ -7,14 +7,16 @@
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+//    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
         '::1',
+        '137.194.2.20',
+        '217.108.141.9',
     ))
 ) {
     header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.<br>HTTP_CLIENT_IP: ('.isset($_SERVER['HTTP_CLIENT_IP']).');<br>HTTP_X_FORWARDED_FOR: ('.isset($_SERVER['HTTP_X_FORWARDED_FOR']).');<br>Your ip is '.@$_SERVER['REMOTE_ADDR']);
 }
 
 require_once __DIR__.'/../app/bootstrap.php.cache';
